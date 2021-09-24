b012c874ea372ed2726ea11e679780602d01f83f
kumar vikrant <vikezme@gmail.com>
2021-09-24 17:56:58 +0930

    adding file

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -11,35 +11,35 @@ Product.init(
   {
     // define columns
     id: {
-     type: DataTypes.INTEGER,
-     allowNull: false,
-     primaryKey: true,
-     autoIncrement: true,
-   },
-   category_id: {
       type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-      }
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    stock: {
-     type: DataTypes.INTEGER,
-     allowNull: false,
-     defaultValue: 10,
-     validate: {
-       isNumeric: true,
-     }
-   },
-   product_name: {
-     type: DataTypes.STRING,
-     allowNull: false,
-   },
-   price: {
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
       }
     }
   },
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # E-commerce
+
+DESCRIPTION:-
+There are several Categories, Tags and Products which are linked to each other as a Back-end using Insomnia. A link of walkthrough video has been provided below to demonstrate how exact this app works.
+
+
+
+LINKS:-
+
+GITHUB LINK - https://github.com/vikezme/E-commerce.git
+SCREENCASTIFY LINK - https://watch.screencastify.com/v/dUkEFTeTlgzOTgc1WxOo
+
+
+
+
+LICENSE:-
+MIT license
+
+Contact:-
+vikezme@gmail.com
+https://github.com/vikezme
